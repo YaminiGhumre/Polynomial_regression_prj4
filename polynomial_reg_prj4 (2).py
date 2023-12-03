b218c32cf0b6a1c46d3a99f34f627c04f8c0a72e
Yamini Ghumre <149946476+YaminiGhumre@users.noreply.github.com>
2023-12-03 22:58:10 +0530

    Add files via upload

---
+++ polynomial_reg_prj4 (2).py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+# In[6]:
+
+
+data = pd.read_csv("E:/Python docs/Position_Salaries.csv")
+data
+
+
+# In[8]:
+
+
+X = data['Level'].values.reshape(-1,1)
+y = data['Salary'].values.reshape(-1,1)
+
+
+# In[25]:
+
+
+sns.scatterplot(x = 'Level', y = 'Salary', data = data)
+
+
+# In[10]:
+
+
+##### Simple Linear Regression ---
+from sklearn.linear_model import LinearRegression
+
+
+# In[11]:
+
+
+lin = LinearRegression()
+print("The model is loaded")
+
+
+# In[12]:
+
+
+lin.fit(X,y)
+print("Training Completed")
+
+
+# In[13]:
+
+
+plt.figure(figsize=(5,3))
+plt.scatter(X,y,color = 'blue')
+plt.plot(X,lin.predict(X), color = 'red')
+
+
+# In[14]:
+
+
+#### Polynomial Regression - Features will get transform into polynomial Fetaures
+#### (degree = 3)
+
+
+# In[15]:
+
+
+from sklearn.preprocessing import PolynomialFeatures
+
+
+# In[16]:
+
+
+poly = PolynomialFeatures(degree = 3)
+print("Loaded the Polynomial Features")
+
+
+# In[17]:
+
+
+X_poly = poly.fit_transform(X)
+X_poly
+
+
+# In[18]:
+
+
+poly.fit(X_poly,y)
+print("Polynomial Features is fitted and Trained")
+
+
+# In[27]:
+
+
+lin2 = LinearRegression()
+
+
+# In[28]:
+
+
+lin2.fit(X_poly,y)
+print("The Model is trained using Polynomial Features")
+
+
+# In[29]:
+
+
+plt.figure(figsize=(5,3))
+plt.scatter(X,y,color = 'blue')
+plt.plot(X,lin2.predict(X_poly), color = 'red')
+
+
+# In[30]:
+
+
+from sklearn.metrics import r2_score
+
+
+# In[31]:
+
+
+r2_score(y,lin.predict(X)) #### rsquare value for Linear Regression
+
+
+# In[32]:
+
+
+r2_score(y,lin2.predict(X_poly)) #### rsquare value for Polynomial Regression
+
+
+# In[34]:
+
+
+new_salary_pred = lin2.predict(poly.fit_transform([[6.5]]))
+
+
+# In[35]:
+
+
+new_salary_pred
+
+
+# In[ ]:
+
+
+
+
